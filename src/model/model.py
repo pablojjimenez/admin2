b5bd6f8f4816931ac4c1fe00ae95f93e45b267da
pablojj1808 <pablojjinfo@gmail.com>
2021-08-31 23:57:22 +0200

    Change method

--- src/model/model.py
+++ src/model/model.py
@@ -3,8 +3,22 @@ from typing import Iterable, Dict
 
 class Model:
     def __init__(self, data: Dict):
-        for i in data.keys():
-            setattr(self, i, data.get(i))
+        if data: self._fill_attrs(data)
 
     def _dir(self) -> Iterable[str]:
         return list(filter(lambda x: x[:1] != '_', self.__dir__()))
+
+    def _fill_attrs(self, data: Dict):
+        for i in data.keys():
+            setattr(self, i, data.get(i))
+
+class IngresoM(Model):
+    def __init__(self, data: Dict = None):
+        self.ingreso = None
+        self.concepto = None
+        self.fecha = None
+        self.pagador = None
+        super().__init__(data)
+
+    def fill(self):
+        print('arreglameeee')
