67a14e7a5c715801b438e8548c9eed86c409772b
pablojj1808 <pablojjinfo@gmail.com>
2021-08-31 23:57:41 +0200

    Add unix terminal color

---
+++ src/color.py
@@ -0,0 +1,22 @@
+class C:
+    RST = "\x1B[0m"
+    KRED = "\x1B[31m"
+    KGRN = "\x1B[32m"
+    KYEL = "\x1B[33m"
+    KBLU = "\x1B[34m"
+    KMAG = "\x1B[35m"
+    KCYN = "\x1B[36m"
+    KWHT = "\x1B[37m"
+
+    FGRN = "\u001B[42m"
+    FRED = "\u001B[41m"
+    FYEL = "\u001B[43m"
+    FBLU = "\u001B[44m"
+    FMAG = "\u001B[45m"
+    FCYN = "\u001B[46m"
+    FWHT = "\u001B[47m"
+    FBLK = "\u001B[40m"
+
+    BOLD = "\x1B[1m"
+    UNDL = "\x1B[4m"
+    CURS = "\x1B[3m"
